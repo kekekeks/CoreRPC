aa291d7d70a9c6d31190494048ba0008604f463c
TocoToucan <TocoToucanMS@gmail.com>
2017-09-21 22:52:32 +0300

    Dispose MemoryStream

--- CoreRPC/Extensions.cs
+++ CoreRPC/Extensions.cs
@@ -11,19 +11,19 @@ namespace CoreRPC
     {
         public static async Task<byte[]> ReadExactlyAsync(this Stream stream, int size)
         {
-            var ms = new MemoryStream(size);
-            var buffer = new byte[8192];
-            while (size > 0)
+            using (var ms = new MemoryStream(size))
             {
-                var read = await stream.ReadAsync(buffer, 0, Math.Min(size, buffer.Length));
-                if (read == 0)
-                    throw new EndOfStreamException();
-                ms.Write(buffer, 0, read);
-                size -= read;
+                var buffer = new byte[8192];
+                while (size > 0)
+                {
+                    var read = await stream.ReadAsync(buffer, 0, Math.Min(size, buffer.Length));
+                    if (read == 0)
+                        throw new EndOfStreamException();
+                    ms.Write(buffer, 0, read);
+                    size -= read;
+                }
+                return ms.ToArray();
             }
-            return ms.ToArray();
         }
-
-
     }
 }

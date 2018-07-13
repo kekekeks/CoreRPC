a6b3550de3404487b9e7498ce0c52cc8d0d758f1
Nikita Tsukanov <keks9n@gmail.com>
2018-07-13 14:19:38 +0300

    Added build script

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied"
+  exit 1
+fi
+VERSION=$1
+cd $DIR
+set -x
+dotnet msbuild /t:Restore /p:Version=$VERSION
+rm -rf build
+mkdir build
+for d in CoreRPC CoreRPC.AspNetCore
+do
+	cd $DIR/$d
+	dotnet msbuild /t:Pack /p:Configuration=Release /p:Version=$VERSION
+	cp bin/Release/*.nupkg $DIR/build
+done
